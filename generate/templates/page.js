d1d7ebe03bfaeed9c827b1550bcf2c1f9a74fbdc
Chakkrit Tunyong <krit.chakkrit@bitkub.com>
2021-01-23 14:51:03 +0700

    update template

--- generate/templates/page.js
+++ generate/templates/page.js
@@ -3,16 +3,22 @@ import { LayoutMain, LayoutPage } from '@layouts';
 import { observer, inject } from 'mobx-react';
 import { Crud } from '@components';
 
-@inject("app")
+const Crud = dynamic(import('../../base/components/crud'), {
+    ssr: false,
+});
+const Layout = dynamic(import('../../base/components/layout'), {
+    ssr: false,
+});
 @observer
 export default class {componentName} extends React.Component {
 
   state = {
-    api:'/api/{databaseName}/{modelName}',
+    api:'/api/main/{databaseName}/{modelName}',
     columns:[{
       title: '{indexName}',
       dataIndex: '{index}',
       sorter: true,
+      render: (item, record) => <span>{record.{index}}</span>,
     }]
   };
 
@@ -25,13 +31,24 @@ export default class {componentName} extends React.Component {
 
   render(){
     return (
-        <LayoutMain {...this.header}>
+      <Layout {...this.header}>
         <Crud 
             header={this.header}
             api={this.state.api}
             columns={this.state.columns}
+
+            readOnly={false}
+            actionOnly={true}
+            fileExport={true}
+
+            scroll={{ x: 1500 }}
+
+            canAdd={false}
+            canEdit={false}
+            canDel={false}
+            pageCustom={true}
         />
-      </LayoutMain>
+      </Layout>
     )
   }
 }
--- generate/templates/service.js
+++ generate/templates/service.js
@@ -29,27 +29,50 @@ const {serviceName} = () => {}
 
 
 {serviceName}.list = async(req, res) => {
-    var
-        data, options,
+    var data,
+        options,
         input = req.query,
-        current = +input.page || 1
+        current = +input.page || 1,
+        search = input.search
+            ? JSON.parse(new Buffer(input.search, 'base64').toString('ascii'))
+            : false,
         conditions = {},
         page = {
+            showQuickJumper:true,
             current: current,
-            pageSize: 10
+            pageSize: 10,
         };
 
+
+        if (search.date_create) {
+            conditions.date_create = {
+                $gte: new Date(search.date_create[0]),
+                $lt: new Date(search.date_create[1]),
+            };
+            delete search.date_create;
+        }
+
+        if (Object.keys(search).length >= 1) {
+            Object.keys(search).map(item => {
+                conditions[item] =
+                    D.{databaseName}.{modelName}.schema.paths[item].instance == 'String'
+                        ? { $regex: search[item], $options: 'i' }
+                        : search[item];
+            });
+        }
+
+        console.log('conditions====>', conditions);
 
         options = {
             limit: page.pageSize,
-            sort: {},
-            skip: (page.current - 1) * page.pageSize
+            sort: {_id: -1},
+            skip: (page.current - 1) * page.pageSize,
         };
 
-        if(input.sortField && input.sortOrder){
-           options.sort[input.sortField] = input.sortOrder=='ascend'? 1 : -1
-        }else{
-            options.sort = {_id: -1}
+        if (input.sortField && input.sortOrder && input.sortField != 'undefined') {
+            options.sort[input.sortField] = input.sortOrder == 'ascend' ? 1 : -1;
+        } else {
+            options.sort = { _id: -1 };
         }
 
         data = await D.{databaseName}.{modelName}.find(conditions, null, options); //.populate('db filed');
@@ -118,5 +141,78 @@ const {serviceName} = () => {}
             msg :'delete false'
         }
 }
+
+AccountService.export = async (req, res) => {
+    try {
+        var data = new Buffer(req.params.query, 'base64').toString('ascii');
+        var data_allow = {};
+        var data_list = [];
+        var data_sheet = [];
+        var return_data = JSON.parse(data);
+        var query_command = '';
+        var query_data = [];
+        if(return_data.full){
+            delete return_data.full
+        }
+        if (return_data.date_create) {
+            var date_select = {
+                date_create: {
+                    $gte: new Date(return_data.date_create[0]),
+                    $lt: new Date(return_data.date_create[1]),
+                },
+            };
+            query_data.push(date_select);
+            delete return_data.date_create;
+        }
+
+        if (Object.keys(return_data).length >= 1) {
+            var option_data = {};
+            Object.keys(return_data).map(item => {
+                if (return_data[item].toString() != '') {
+                    option_data[item] = return_data[item];
+                }
+            });
+            if (option_data != {} && Object.keys(option_data).length >= 1) {
+                query_data.push(option_data);
+            }
+        }
+
+        query_command =
+            query_data.length >= 2
+                ? { $and: query_data }
+                : query_data[0]
+                ? query_data[0]
+                : {};
+        data_allow = {
+            _id: 1,
+        };
+        data_list = await D.{databaseName}.{modelName}.find(query_command);
+        data_sheet = [Object.keys(data_allow)].concat(
+            data_list.map(item => {
+                return Object.keys(data_allow).map(node => {
+                    return item._doc[node] ? item._doc[node].toString() : '';
+                });
+            })
+        );
+
+        var wb = F.excel.utils.book_new();
+        F.excel.utils.book_append_sheet(
+            wb,
+            F.excel.utils.aoa_to_sheet(data_sheet),
+            '{modelName}'
+        );
+
+        return {
+            type: 'xlsx',
+            data: F.excel.write(wb, {
+                type: 'buffer',
+                bookType: 'xlsx',
+            }),
+        };
+    } catch (err) {
+        console.log('err==>', err);
+        return { type: 'excel', data: [] };
+    }
+};
 
 module.exports = {serviceName};
